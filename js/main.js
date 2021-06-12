ad18edf10c87d4a2f852ed095ba8100368adc636
silasAsamoah <three.chapter.me@gmail.com>
2021-06-12 21:47:13 +0000

    portfolio not swiping bug

--- js/main.js
+++ js/main.js
@@ -96,3 +96,18 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+
+// PORTFOLIO --swiper
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
