6556ef50fb0d0acc7b734873ddb83cdb5837cafc
silasAsamoah <three.chapter.me@gmail.com>
2021-06-16 17:35:37 +0000

    done with all -goUp not done

--- js/main.js
+++ js/main.js
@@ -136,3 +136,25 @@ let swiperTest = new Swiper(".testimonial__container", {
         }
     }
   });
+
+
+
+//   SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+     
+    sections.forEach(current=> {
+        const sectionHeight = current.offsetHeight
+        const sectionTop= current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active__link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active__link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
